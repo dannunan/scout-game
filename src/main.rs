623ba053c00c9385e02f2544131aff12ac6ba199
Dan <dannunan6119@gmail.com>
2023-11-02 18:22:15 +0000

    Removed CLI parsing

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,19 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-use scout::Config;
-use std::env;
 use std::process;
 
 fn main() {
     println!("Scout!");
 
-    let args: Vec<String> = env::args().collect();
+    let strategies: Vec<scout::Strategy> = vec![
+        scout::get_player_action,
+        scout::get_player_action,
+        scout::get_player_action,
+        scout::get_player_action,
+    ];
 
-    if args.len() < 1 {
-        println!("Missing arguments: n_players");
-        process::exit(1);
-    }
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Error parsing arguments: {}", err);
-        process::exit(1);
-    });
-
-    if let Err(e) = scout::run(config) {
+    if let Err(e) = scout::run(strategies) {
         println!("Application error: {}", e);
         process::exit(1);
     }
