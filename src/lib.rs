96269bdc4f541251e0c5b9028a888da7440ee249
Dan <dannunan6119@gmail.com>
2023-11-04 12:43:58 +0000

    Reordered module

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,25 @@ use rand::thread_rng;
 use std::collections::{HashMap, VecDeque};
 use std::{error::Error, io};
 
+#[derive(Debug, Clone)]
+pub struct Card(i32, i32);
+
+impl Card {
+    fn flip(&self) -> Card {
+        Card(self.1, self.0)
+    }
+}
+
+type Set = VecDeque<Card>;
+
+/// Each player has a hand, some points, and their "Scout show" move.
+#[derive(Debug, Default, Clone)]
+pub struct Player {
+    hand: Set,
+    score: i32,
+    scout_show: bool,
+}
+
 /// Player actions on a given turn.
 pub enum Action {
     /// Scouting moves a card from the active set into the hand (it may be flipped)
@@ -19,107 +38,13 @@ pub enum Action {
     ScoutShow(bool, bool, usize, usize, usize),
 }
 
-#[derive(Debug, Clone)]
-pub struct Card(i32, i32);
-
-impl Card {
-    fn flip(&self) -> Card {
-        Card(self.1, self.0)
-    }
-}
-impl std::fmt::Display for Card {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "({}, {})", self.0, self.1)
-    }
-}
-
-type Set = VecDeque<Card>;
-
-pub fn generate_set_map() -> HashMap<Vec<i32>, i32> {
-    // Generate all legal sets, and assign an i32 value to each.
-    // This is done by generating the sets in order of their value
-
-    // All single card sets
-    // All runs of len 2
-
-    let mut map = HashMap::new();
-    let mut i = 0;
-
-    // For size=1 straights and flushes are identical
-    for base in 1..10 {
-        i = i + 1;
-        map.insert(vec![base], i);
-    }
-
-    // Iterate up to max Set size
-    for size in 2..10 {
-        // First add all straights (ascending and descending)
-        for base in 1..10 {
-            i = i + 1;
-            map.insert((base..=size).collect(), i);
-            map.insert((base..=size).rev().collect(), i);
-        }
-        // Then add all flushes (this is done after to preserve order)
-        for base in 1..10 {
-            i = i + 1;
-            map.insert(vec![base; size.try_into().unwrap()], i);
-        }
-    }
-
-    return map;
-}
-
-/// Each player has a hand, some points, and their "Scout show" move.
-#[derive(Debug, Default, Clone)]
-pub struct Player {
-    hand: Set,
-    score: i32,
-    scout_show: bool,
-}
-
-/// A single instance of a Scout game.
+/// GameState for storing state and generating new state with Actions
 #[derive(Debug, Default)]
 pub struct GameState {
     players: VecDeque<Player>,
     game_size: usize,
     active: Set,
     active_owner: usize,
-}
-
-fn create_deck(game_size: usize, shuffle: bool) -> Set {
-    let mut deck: Set = VecDeque::new();
-    match game_size {
-        3 => {
-            // Each unique combination of 0-8, excluding matches
-            for bottom in 0..9 {
-                for top in 0..bottom {
-                    deck.push_back(Card(top, bottom));
-                }
-            }
-        }
-        4 => {
-            // Each unique combination of 0-9, excluding matches and (9/8)
-            for bottom in 0..10 {
-                for top in 0..bottom {
-                    deck.push_back(Card(top, bottom));
-                }
-            }
-            deck.pop_back();
-        }
-        5 => {
-            // Each unique combination of 0-9, excluding matches
-            for bottom in 0..10 {
-                for top in 0..bottom {
-                    deck.push_back(Card(top, bottom));
-                }
-            }
-        }
-        _ => {}
-    }
-    if shuffle {
-        deck.make_contiguous().shuffle(&mut thread_rng());
-    }
-    return deck;
 }
 
 pub enum NewGameState {
@@ -217,7 +142,7 @@ impl GameState {
                 scores.push(state.players[i].score - state.players[i].hand.len() as i32);
             }
             return NewGameState::GameOver(scores);
-        // Round ends if active player is next player
+            // Round ends if active player is next player
         } else if state.active_owner == 1 {
             // The next player isn't penalised for hand size -
             // in this case, offset this players points by hand size
@@ -250,9 +175,94 @@ fn print_set(set: &Set) {
     print!("\n");
 }
 
-// Strategies are ways of generating Actions based on GameState
-
+/// Strategies are ways of generating Actions based on GameState
 pub type Strategy = fn(&GameState) -> Action;
+
+pub fn run(strategies: Vec<Strategy>) -> Result<Vec<i32>, Box<dyn Error>> {
+    let n_players = strategies.len();
+    let mut game = GameState::new(n_players, true);
+    let mut turn = 0;
+
+    loop {
+        let action = get_player_action(&game);
+        match game.take_action(&action) {
+            NewGameState::Continue(new) => game = new,
+            NewGameState::GameOver(mut scores) => {
+                scores.rotate_left(turn);
+                return Ok(scores);
+            }
+        };
+        game.rotate_left();
+        turn = (turn + 1) % n_players;
+    }
+}
+
+fn create_deck(game_size: usize, shuffle: bool) -> Set {
+    let mut deck: Set = VecDeque::new();
+    match game_size {
+        3 => {
+            // Each unique combination of 0-8, excluding matches
+            for bottom in 0..9 {
+                for top in 0..bottom {
+                    deck.push_back(Card(top, bottom));
+                }
+            }
+        }
+        4 => {
+            // Each unique combination of 0-9, excluding matches and (9/8)
+            for bottom in 0..10 {
+                for top in 0..bottom {
+                    deck.push_back(Card(top, bottom));
+                }
+            }
+            deck.pop_back();
+        }
+        5 => {
+            // Each unique combination of 0-9, excluding matches
+            for bottom in 0..10 {
+                for top in 0..bottom {
+                    deck.push_back(Card(top, bottom));
+                }
+            }
+        }
+        _ => {}
+    }
+    if shuffle {
+        deck.make_contiguous().shuffle(&mut thread_rng());
+    }
+    return deck;
+}
+
+pub fn generate_set_map() -> HashMap<Vec<i32>, i32> {
+    // Generate all legal sets, and assign an i32 value to each.
+    // This is done by generating the sets in order of their value
+
+    let mut map = HashMap::new();
+    let mut i = 0;
+
+    // For size=1 straights and flushes are identical
+    for base in 1..10 {
+        i = i + 1;
+        map.insert(vec![base], i);
+    }
+
+    // Iterate up to max Set size
+    for size in 2..10 {
+        // First add all straights (ascending and descending)
+        for base in 1..10 {
+            i = i + 1;
+            map.insert((base..=size).collect(), i);
+            map.insert((base..=size).rev().collect(), i);
+        }
+        // Then add all flushes (this is done after to preserve order)
+        for base in 1..10 {
+            i = i + 1;
+            map.insert(vec![base; size.try_into().unwrap()], i);
+        }
+    }
+
+    return map;
+}
 
 pub fn get_player_action(state: &GameState) -> Action {
     // Print some info
@@ -274,30 +284,6 @@ pub fn get_player_action(state: &GameState) -> Action {
             println!("Not a valid action! Enter: Scout, Show, Scout and show, or Quit");
             get_player_action(state)
         }
-    }
-}
-
-pub struct Game {
-    state: GameState,
-    strategies: Vec<Strategy>,
-}
-
-pub fn run(strategies: Vec<Strategy>) -> Result<Vec<i32>, Box<dyn Error>> {
-    let n_players = strategies.len();
-    let mut game = GameState::new(n_players, true);
-    let mut turn = 0;
-
-    loop {
-        let action = get_player_action(&game);
-        match game.take_action(&action) {
-            NewGameState::Continue(new) => game = new,
-            NewGameState::GameOver(mut scores) => {
-                scores.rotate_left(turn);
-                return Ok(scores);
-            }
-        };
-        game.rotate_left();
-        turn = (turn + 1) % n_players;
     }
 }
 
